522c7a7f01a11ca5ffae04cf1762f4b57557417b
Jono Hawkins <jonathan.hawkins.17@ucl.ac.uk>
2024-04-24 13:02:23 +0100

    updates setup.py to include requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 from setuptools import setup, find_packages
 
+install_requires=[
+    'contourpy==1.2.1',
+    'cycler==0.12.1',
+    'fonttools==4.50.0',
+    'kiwisolver==1.4.5',
+    'matplotlib==3.8.4',
+    'numpy==1.26.4',
+    'packaging==24.0',
+    'pillow==10.3.0',
+    'pyparsing==3.1.2',
+    'pyserial==3.5',
+    'python-dateutil==2.9.0.post0',
+    'six==1.16.0'
+]
+
 setup(
     name="cryoskills",
     # Configure setuptools to look for packages in src/
@@ -15,5 +30,6 @@ setup(
         "gui_scripts" : [
             "cryoskills-receiver = cryoskills:launch_new_receiver_gui"
         ]
-    }
+    },
+    install_requires = install_requires
 )
